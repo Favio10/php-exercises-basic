1489892648ee6d6c65a2cb52bbebcd840ba3f8cd
Favio10 <favio_olivera@hotmail.com>
2024-09-09 19:53:28 -0300

    eje4

--- Nivel 1/eje5.php
+++ Nivel 1/eje5.php
@@ -1,2 +1,15 @@
+<?php
 # Consigna: Crea dos arrays, uno con números impares y otro con números pares. Agrega un nuevo número al array de pares. Luego, mezcla ambos arrays en uno solo y muestra la longitud del array resultante. Finalmente, imprime el contenido del array mezclado valor por valor.
 # Hint: Puedes usar array_push() para agregar un valor a un array, y array_merge() para combinar los dos arrays.
+
+$array1 = [1, 3, 5, 7, 9];
+$array2 = [2, 4, 6, 8];
+
+$array2[] = 0;
+
+$nuevo_arr = array_merge($array1, $array2);
+echo "La longitus del array es: ". count($nuevo_arr);
+
+foreach ($nuevo_arr as $valor) {
+    echo $valor . " ";
+}
