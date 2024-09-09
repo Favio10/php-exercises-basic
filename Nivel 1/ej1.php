2d8726fde71a8aef92441db88233db360f4ab59c
Favio10 <favio_olivera@hotmail.com>
2024-09-09 18:58:54 -0300

    consignas

--- Nivel 1/ej1.php
+++ Nivel 1/ej1.php
@@ -1,2 +1,11 @@
+<?php
 # Consigna: Define una variable de cada tipo de datos: entero, doble, string y booleano. Luego, imprime cada una en la pantalla.
 # Hint: Puedes utilizar echo para imprimir las variables y concatenar texto para que se vea más descriptivo.
+
+$entero = 1234;
+$doble = 13.5;
+$string = "hola que tal";
+$bool = true;
+
+echo "Tu entero es: $entero, el flotante es: $doble, tambien: $string, y por ultimo: $bool";
+?>
--- Nivel 1/eje2.php
+++ Nivel 1/eje2.php
@@ -1,2 +1,32 @@
+<?php
 # Consigna: Tienes una frase almacenada en la variable $mensaje. Crea tres funciones: una que convierta la frase a mayúsculas, otra que calcule su longitud y otra que la invierta. Finalmente, imprime los resultados de cada función y la frase original concatenada con otra frase.
 # Hint: Usa funciones nativas de PHP como strtoupper(), strlen() y strrev().
+
+$mensaje = "Hola. Bienvenidos!";
+
+function mayusc($string){
+    $mayus = strtoupper($string);
+    echo " Frase en mayusculas: $mayus<br>";
+};
+
+function long($string){
+    $larg = strlen($string);
+    echo "Longitud de la frase: $larg<br>";
+};
+
+function invier($string){
+    $inv = strrev($string);
+    echo "Frase invertida: $inv<br>";
+};
+
+mayusc($mensaje);
+long($mensaje);
+invier($mensaje);
+
+$otro_msj = "Estaras viendo los resultados de las func =D";
+
+echo "$mensaje $otro_msj"; 
+
+
+
+?>
